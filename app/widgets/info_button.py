291d1b5913f388cb99bef90ebc52ba16c3c2d7c7
Priyanshu Maity <priyanshu.maity2006@gmail.com>
2026-06-10 14:13:31 +0530

    refactor: allow arguments in render() function

--- app/widgets/info_button.py
+++ app/widgets/info_button.py
@@ -1,15 +1,25 @@
 import dash_mantine_components as dmc
 from dash_iconify import DashIconify
 
+from typing import Literal
+
 
 class InfoButton:
     def __init__(self, tooltip: str, width: int = 18):
         self.tooltip = tooltip
         self.width = width
 
-    def render(self):
+    def render(
+            self,
+            position: Literal['top', 'left', 'bottom', 'right', 'top-end', 'top-start', 'left-end', 'left-start', 'bottom-end', 'bottom-start', 'right-end', 'right-start'] | None = 'right-start',
+            transition: str | None = 'pop-top-left',
+            with_arrow: bool = True,
+            multiline: bool = True,
+            w: int = 200,
+    ):
         return dmc.Tooltip(
             label=self.tooltip,
+            color='background.8',
             children=dmc.ActionIcon(
                 DashIconify(
                     icon='tabler:info-circle',
@@ -17,12 +27,12 @@ class InfoButton:
                 ),
                 variant='subtle',
             ),
-            position='right-start',
-            withArrow=True,
-            multiline=True,
-            w=200,
+            position=position,
+            withArrow=with_arrow,
+            multiline=multiline,
+            w=w,
             transitionProps={
-                'transition': 'pop-top-left',
+                'transition': transition,
                 'duration': 200,
                 'timingFunction': 'ease',
             },
